45b0cc6869d736a7e416e92d8e07ba96232b7200
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2023-04-09 15:18:27 -0400

    added burger menu

--- js/main.js
+++ js/main.js
@@ -37,3 +37,11 @@ buttons.forEach((button) => {
     imageElement.src = teamMembers[index].image;
   });
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("show");
+});
